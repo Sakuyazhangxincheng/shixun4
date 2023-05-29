fc08490326993736fcfdb259e03efd389d9cc1f8
ZXC <1449722625@qq.com>
2023-05-29 11:20:17 +0800

    5.29

---
+++ vue/shared_bike/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <el-button @click="startHacking">element ui 安装成功</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: 'It works!',
+        type: 'success',
+        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+        duration: 5000
+      })
+    }
+  },
+  components:{
+    UserInfo
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+</style>
---
+++ vue/shared_bike/src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+
+import axios from 'axios'
+import moment  from 'moment'
+
+
+Vue.filter('moment', function (value, formatString) {
+  formatString = formatString || 'YYYY-MM-DD HH:mm:ss';
+  return moment(value).format("YYYY-MM-DD"); // value可以是普通日期 20170723
+});
+
+Vue.prototype.$axios = axios
+
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
